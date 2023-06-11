64c7c5870db3e5f3aa43f029b834f36a0c153ae3
Moskovojs <rihards.moskovojs05@gmail.com>
2023-06-11 18:15:51 +0300

    Noversta problema kur programma pec katra atbildeta jautajuma neparsledzas un nakosajiem jautajumiem

--- Assets/Scripts/ChangeButtonImage.cs
+++ Assets/Scripts/ChangeButtonImage.cs
@@ -10,6 +10,10 @@ public class ChangeButtonImage : MonoBehaviour
     public Button button2;
     public Button button3;
     public Button button4;
+    public Text button1txt;
+    public Text button2txt;
+    public Text button3txt;
+    public Text button4txt;
     public Sprite img1;
     public Sprite img2;
     public Sprite img3;
@@ -25,30 +29,7 @@ public class ChangeButtonImage : MonoBehaviour
     private bool button4Clicked = false;
     void Update()
     {
-        
-        // if (button1Clicked)
-        // {
-        //     Debug.Log("Button 1 clicked!");
-        //     button1Clicked = false;
-        // }
-
-        // if (button2Clicked)
-        // {
-        //     Debug.Log("Button 2 clicked!");
-        //     button2Clicked = false;
-        // }
-
-        // if (button3Clicked)
-        // {
-        //     Debug.Log("Button 3 clicked!");
-        //     button3Clicked = false;
-        // }
-
-        // if (button4Clicked)
-        // {
-        //     Debug.Log("Button 4 clicked!");
-        //     button4Clicked = false;
-        // }
+      
     }
 
     public void OnButton1Click()
@@ -102,67 +83,66 @@ public class ChangeButtonImage : MonoBehaviour
         button3.interactable = true;
         button4.interactable = true;
     }
+    //3 sekundes parada pareizo atbildi
+    private IEnumerator ShowCorrectAnswers(){
+        yield return new WaitForSeconds(3f);
+        if(q1==true && q2==false){
+        Q2Load();
+        }else if(q2==true && q3==false){
+        Q3Load();
+        }
+        
+    }
     private void Questions(){
+//pirmais jautajms
         if(q1==false){
             if(button1Clicked==true){
                 score++;
-                button1.image.sprite = img2;
+                
                 q1 = true;
             }else{
-                button1.image.sprite = img2;
+                
                 q1 = true;
             }
-            StartCoroutine(LaiksKamerPogaAtslegta());
-            Q2Load();
-
+            button1.image.sprite = img2;
+//otrais jautajums
         }else if(q2==false){
             if(button2Clicked==true){
                 score++;
-                button2.image.sprite = img2;
-                q2=true ;
+                
+                q2=true;
             }else{
-                button2.image.sprite = img2;
+               
                 q2=true;
             }
-            StartCoroutine(LaiksKamerPogaAtslegta());
-            
-
+            button2.image.sprite=img2;
+//tresais jautajums
         }else if(q3==false){
 
+        }else if(q4==false){
+
+        }else if(q5==false){
+
         }
+        StartCoroutine(LaiksKamerPogaAtslegta());
+            StartCoroutine(ShowCorrectAnswers());
     }
+    //ielade otro jautajumu
     private void Q2Load(){
     questionText.text = "celebrant?";
+    button1txt.text="jatbildd";
+    button2txt.text = "bbbb";
+    button3txt.text = "ddd";
+    button4txt.text = "eee12331";
     }
-        
-
-
-
-
-    // private void ChangeImage(Button button)
-    // {
-    //     clickedButton = button;
-
-    //     clickedButton.image.sprite = img1;
-    //     StartCoroutine(DelayedChangeImage(clickedButton));
-
-    //     button1.interactable = false;
-    //     button2.interactable = false;
-    //     button3.interactable = false;
-    //     button4.interactable = false;
-    // }
-    
-    // private IEnumerator DelayedChangeImage(Button button)
-    // {
-    //     yield return new WaitForSeconds(3f);
-        
-    //     button.image.sprite = img2;
-
-    //     button1.interactable = true;
-    //     button2.interactable = true;
-    //     button3.interactable = true;
-    //     button4.interactable = true;
-    // }
+    //ielade treso jautajumu
+    private void Q3Load(){
+        questionText.text = "Pareizs termins ciklam ar pecnosacijumu?";
+    button1txt.text="ff";
+    button2txt.text = "aa";
+    button3txt.text = "3";
+    button4txt.text = "tt";
+    }
 }
 
 
